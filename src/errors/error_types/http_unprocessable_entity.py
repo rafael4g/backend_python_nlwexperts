8f0ac704c4b9644a1be182d3f8cc05af8aab4b98
Rafael D Silva <neromad@outlook.com>
2024-02-08 14:24:43 -0300

    feat: Adding validator with cerberus

---
+++ src/errors/error_types/http_unprocessable_entity.py
@@ -0,0 +1,8 @@
+class HttpUnprocessableEntityError(Exception):
+
+    def __init__(self, message: str) -> None:
+        super().__init__(message)
+        self.message = message
+        self.name = "unprocessableEntity"
+        self.status_code = 422
+        
