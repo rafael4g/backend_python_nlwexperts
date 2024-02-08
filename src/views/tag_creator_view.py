f6de532d2599e095c6c66cadfce86dff0e404c15
Rafael D Silva <neromad@outlook.com>
2024-02-08 12:16:29 -0300

    feat: Implementing Controllers

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,5 +1,6 @@
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
+from src.controllers.tag_creator_controller import TagCreatorController
 
 class TagCreatorView:
     '''
@@ -7,10 +8,12 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
-        #body = http_request.body
-        #product_code = body['product_code']
+        tag_creator_controller = TagCreatorController()
 
-        print('Estou na minha View')
-        print(http_request)
+        body = http_request.body
+        product_code = body['product_code']
 
-        return HttpResponse(status_code=200, body={'Hello': 'world'})
+        formatted_response = tag_creator_controller.create(product_code)
+
+
+        return HttpResponse(status_code=200, body=formatted_response)
